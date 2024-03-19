bfa11940085c9495c65702b59ec2b7b831d5d62d
HaileInnoTech <hai28022002@gmail.com>
2024-03-19 20:36:06 +0700

    Update Login.vue

--- src/components/Pages/Login.vue
+++ src/components/Pages/Login.vue
@@ -72,13 +72,12 @@
           </div>
 
           <div class="mt-6 flex justify-center">
-            <button
+            <router-link to="/dashboard/employee"
               type="button"
               class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-              @click="loginHandleSwal2"
             >
               Login
-            </button>
+            </router-link>
           </div>
         </div>
       </div>
